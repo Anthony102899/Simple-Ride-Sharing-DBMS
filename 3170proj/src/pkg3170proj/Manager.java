7632e88398b8f6b94318e3275dbd5db22df48f9c
Anthony102899 <37758485+Anthony102899@users.noreply.github.com>
2020-12-02 22:54:55 +0800

    Add files via upload

--- 3170proj/src/pkg3170proj/Manager.java
+++ 3170proj/src/pkg3170proj/Manager.java
@@ -1,16 +1,103 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg3170proj;
 import java.sql.*;
+import java.util.*;
 /**
  *
- * @author 24111
+ * @author LYU An
  */
 public class Manager {
     public Manager(Connection con){
-        
+        int temp = 0;
+        while(temp != 2){
+            temp = Manager_UI();
+            switch(temp){
+                case 1:
+                    try{
+                        findTrips(con);
+                    }catch(Exception e){
+                        
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("[ERROR]: invalid input!");
+            }
+        }
+    }
+    
+    public static int Manager_UI(){
+        System.out.println("Manager, what would you like to do?");
+        System.out.println("1. Find trips");
+        System.out.println("2. Go back");
+        System.out.println("Please enter [1-2]");
+        int m;
+        Scanner scn = new Scanner(System.in);
+        try{
+            m = scn.nextInt();
+        }catch(Exception e){
+            m = 0;
+        }
+        return m;
+    }
+    
+    public static void findTrips(Connection con) throws SQLException{
+        int dis_min = -1;
+        int dis_max = -1;
+        int flag = 0;
+        while(dis_min < 0){
+            dis_min = getMin(); 
+            if(dis_min < 0){
+                System.out.println("[ERROR]: The input should be an integer no smaller than 0.");
+            }
+        }
+        while(dis_max < 0 || dis_max <= dis_min){
+            dis_max = getMax();
+            if(dis_max <= 0 || dis_max <= dis_min){
+                System.out.println("[ERROR]: The input should be a positive integer and larger than the minimum.");
+            }
+        }
+        //System.out.println(dis_min + ", " + dis_max);
+        Statement stmt = con.createStatement();
+        String SelectQuery = "SELECT t.id, d.name, p.name, s1.name, s2.name, ROUND(TIMESTAMPDIFF(SECOND,t.start_time,t.end_time)/60) AS duration FROM trip t, passenger p, driver d, taxi_stop s1, taxi_stop s2 "
+                                     + "WHERE t.start_location = s1.name "
+                                     + "AND t.destination = s2.name "
+                                     + "AND t.driver_id = d.id "
+                                     + "AND t.passenger_id = p.id "
+                                     + "AND (abs(s1.location_x - s2.location_x) + abs(s1.location_y - s2.location_y)) BETWEEN " + dis_min + " AND " + dis_max +";"
+                                    ;
+        try{
+            ResultSet rs = stmt.executeQuery(SelectQuery);
+            System.out.println("trip id, driver name, passenger name, start location, destination, duration");
+            while(rs.next()){
+                System.out.println(rs.getString(1) + ", " + rs.getString(2) + ", " + rs.getString(3) + ", " + rs.getString(4) + "," + rs.getString(5) + ", " + rs.getString(6));
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+    public static int getMin(){
+        int min;
+        System.out.println("Please enter the minimum traveling distance.");
+        Scanner scn = new Scanner(System.in);
+        try{
+            min = scn.nextInt();
+        }catch(Exception e){
+            min = -1;
+        }
+        return min;
+    }
+    public static int getMax(){
+        int max;
+        System.out.println("Please enter the maximum traveling distance.");
+        Scanner scn = new Scanner(System.in);
+        try{
+            max = scn.nextInt();
+        }catch(Exception e){
+            max = -1;
+        }
+        return max;
     }
 }
